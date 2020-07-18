ea4817f22bd4872c2d6e7f469c0570837becb9d4
mkosturek <35629146+mkosturek@users.noreply.github.com>
2020-07-18 20:02:39 +0200

    Added autoenv installation to instal_basics

--- install-basics.sh
+++ install-basics.sh
@@ -4,3 +4,7 @@ sudo apt install vim git guake tmux htop
 sudo apt install exfat-fuse exfat-utils
 
 sudo apt-get install openssh-server
+
+# AutoenvA
+git clone git://github.com/inishchith/autoenv.git ~/.autoenv
+echo 'source ~/.autoenv/activate.sh' >> ~/.bashrc
